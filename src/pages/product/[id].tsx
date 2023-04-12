9a771641a9c5c0c25876d465bebe767b5ff2fd19
Almir Jr <almagomes1@hotmail.com>
2023-04-12 09:21:01 -0300

    error

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,6 @@
 
 import { ImagemContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
-import { GetStaticProps } from "next"
+import { GetStaticPaths, GetStaticProps } from "next"
 import { stripe } from "../../lib/stripe"
 import Stripe from "stripe"
 import Image from "next/image"
@@ -37,31 +37,36 @@ export default function Product({ products }: ProductProps) {
     )
 }
 
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [
+            { params: { id: 'prod_NeoVepfJBFzQnb' }}
+        ],
+        fallback: false,
+    }
+}
+    
 export const getStaticProps: GetStaticProps<any, {id: string}> = async ({ params }) => {
     const productId = params.id;
     const product = await stripe.products.retrieve(productId, {
         expand: ['default_price'],
     });
 
-    
-
     const price = product.default_price as Stripe.Price
-    return 
-  }
     
-    return {
-        props: {
-            product: {
-                id: product.id,
-                name: product.name,
-                imageUrl: product.images[0],
-                price: new Intl.NumberFormat('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL',
-                }).format(price.unit_amount / 100),
-                description: product.description,
-              }
-        },
-        revalidate: 60 * 60 * 1,    
+   return {
+    props: {
+        product: {
+            id: product.id,
+            name: product.name,
+            imageUrl: product.images[0],
+            price: new Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+            }).format(price.unit_amount / 100),
+            description: product.description,
+        }
     },
+    revalidate: 60 * 60 * 1
+   }
 }
